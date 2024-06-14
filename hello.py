6e9f678a445077d1be8f83f2622b42534b94cc0d
adsz <adamszydlo@gmail.com>
2024-06-15 01:51:10 +0200

    Modified files

--- hello.py
+++ hello.py
@@ -1,7 +1,17 @@
-from flask import Flask
+from flask import Flask, jsonify
+import subprocess
 
 app = Flask(__name__)
 
-@app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+
+@app.route('/')
+def hello():
+    return 'Hello, World!'
+
+
+@app.route('/packages')
+def list_packages():
+    result = subprocess.run(['pip', 'list'], capture_output=True, text=True)
+    packages = result.stdout
+    return f'<pre>{packages}</pre>'
+
